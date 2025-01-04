924e524b3fa2f36623ca306a164a42171c4ecc12
sean dougherty <sd@kexp.org>
2025-01-04 11:44:33 -1000

    able to parse strings and use them in the repl now

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -48,6 +48,9 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		env.Set(node.Name.Value, val)
 
 	// Expressions
+	case *ast.StringLiteral:
+		return &object.String{Value: node.Value}
+		
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
 
